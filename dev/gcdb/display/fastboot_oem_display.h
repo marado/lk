37ed6bd93a2b3c5f2926ca19f66ca03178fdc684
Rashi Bindra <rbindra@codeaurora.org>
2018-01-15 12:00:43 +0530

    dev: gcdb: display: add support for FHD+ video mode panel selection
    Add support to select FHD+ video mode panel via oem display override
    command. The following command can be used
    - fastboot oem select-display-panel lgd_incell_sw49106_fhd_video:skip
    
    Change-Id: I99cde947a266d4fdbc782cc7b2d8b4b02daacd4a
    Signed-off-by: Rashi Bindra <rbindra@codeaurora.org>

--- dev/gcdb/display/fastboot_oem_display.h
+++ dev/gcdb/display/fastboot_oem_display.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2016, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2018, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -110,6 +110,8 @@ struct panel_lookup_list lookup_skip_panels[] = {
 	{"sharp_1080p_cmd", "qcom,mdss_dsi_sharp_1080p_cmd", false},
 	{"sharp_120hz_1080p_cmd", "qcom,mdss_dual_sharp_1080p_120hz_cmd",
 		true},
+	{"lgd_incell_sw49106_fhd_video", "qcom,mdss_dsi_lgd_incell_sw49106_fhd_video",
+		false},
 };
 
 struct sim_lookup_list lookup_sim[] = {
