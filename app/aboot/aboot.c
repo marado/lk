210d41c8c31ccfcab0a741bc81f353e9715ffce0
vkakani <vkakani@codeaurora.org>
2019-01-03 18:53:49 +0530

    cmdline: set mem= boot argument dynamically
    To set mem= boot argument, use
    fastboot oem boot_memory <MEM>
    
    Change-Id: I01a0f12498ff89be0fa8071a73b614f1659a9ae7
    Signed-off-by: vkakani <vkakani@codeaurora.org>

--- app/aboot/aboot.c
+++ app/aboot/aboot.c
@@ -212,6 +212,7 @@ static const char *resume = " resume=/dev/mmcblk0p";
 static const char *golden_image = " golden_image";
 #endif
 #endif
+static const char *mem_arg = " mem=";
 #ifdef INIT_BIN_LE
 static const char *sys_path_cmdline = " rootwait ro init="INIT_BIN_LE;
 #endif
@@ -272,7 +273,7 @@ static unsigned long long int blank_img_header_mmc;
 
 /* Assuming unauthorized kernel image by default */
 static int auth_kernel_img = 0;
-static device_info device = {DEVICE_MAGIC,0,0,0,0,0,{0},{0},{0},1,0,0,{0},0,{0},0,0};
+static device_info device = {DEVICE_MAGIC,0,0,0,0,0,{0},{0},{0},1,0,0,{0},0,{0},0,0,{0}};
 static char *vbcmdline;
 static bootinfo info = {0};
 
@@ -438,6 +439,7 @@ unsigned char *update_cmdline(const char * cmdline)
         int syspath_buflen = strlen(sys_path) + sizeof(int) + 2; /*allocate buflen for largest possible string*/
 #endif
 	char syspath_buf[syspath_buflen];
+	int memarg_length = 0;
 #if HIBERNATION_SUPPORT
 	int resume_buflen = strlen(resume) + sizeof(int) + 2;
 	char resume_buf[resume_buflen];
@@ -659,6 +661,11 @@ unsigned char *update_cmdline(const char * cmdline)
 		}
 	}
 #endif
+	if((memarg_length = strlen((char*) device.boot_memory)) != 0)
+	{
+		cmdline_len += strlen(mem_arg);
+		cmdline_len += memarg_length;
+	}
 #if TARGET_CMDLINE_SUPPORT
 	char *target_cmdline_buf = malloc(TARGET_MAX_CMDLNBUF);
 	int target_cmd_line_len;
@@ -931,7 +938,15 @@ unsigned char *update_cmdline(const char * cmdline)
 #endif
 		}
 #endif
-
+		if(memarg_length)
+		{
+			src = mem_arg;
+			--dst;
+			while ((*dst++ = *src++));
+			src = (char *) device.boot_memory;
+			--dst;src++;
+			while ((*dst++ = *src++));
+		}
 #if TARGET_CMDLINE_SUPPORT
 		if (target_cmdline_buf && target_cmd_line_len)
 		{
@@ -4278,6 +4293,35 @@ void cmd_oem_hibernation(const char *arg, void *data, unsigned sz)
 	}
 }
 #endif
+void cmd_oem_boot_memory(const char *arg, void *data, unsigned sz)
+{
+	char *sp = (char *)arg;
+	int length= strlen(sp);
+	switch(*(sp+length-1))
+	{
+		case 'G':
+		case 'g':
+		case 'M':
+		case 'm':
+			if(length < DEVICE_MEMORY_SIZE)
+			{
+				memset(device.boot_memory,0,sizeof(device.boot_memory));
+				sprintf((char*)device.boot_memory,"%s",sp);
+				write_device_info(&device);
+				dprintf(INFO,"set mem=%s\n",device.boot_memory);
+				fastboot_okay("");
+			}
+			else
+			{
+				fastboot_fail("");
+				dprintf(INFO,"Invalid length of memory, Represent memory in higher/lower dimentions\n");
+			}
+			break;
+		default :
+			dprintf(INFO, "Represent memory in M/G \n");
+			fastboot_fail("");
+	}
+}
 void cmd_reboot_bootloader(const char *arg, void *data, unsigned sz)
 {
 	dprintf(INFO, "rebooting the device\n");
@@ -4928,6 +4972,7 @@ void aboot_fastboot_register_commands(void)
 #if HIBERNATION_SUPPORT
 						{"oem hibernation", cmd_oem_hibernation},
 #endif
+						{"oem boot-memory", cmd_oem_boot_memory},
 #if UNITTEST_FW_SUPPORT
 						{"oem run-tests", cmd_oem_runtests},
 #endif
