c71c3a43dca30ccf1e04123161d70237edd1be18
Mayank Grover <groverm@codeaurora.org>
2019-02-05 16:29:18 +0530

    platform: mdm9640: Initialize the scm as part of platform init.
    Initialize scm infrastructure as part of platform intialization.
    
    Change-Id: Ie13e30642886d823ab0db68b8234de1f686185ee

--- platform/mdm9640/platform.c
+++ platform/mdm9640/platform.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2015,2019 The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -90,6 +90,8 @@ void platform_early_init(void)
 
 	/* timer */
 	qtimer_init();
+
+	scm_init();
 }
 
 void platform_init(void)
