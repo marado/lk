907fca1d6f9cf8dac019147896b2752fc188c950
Deepak Kalgudi <DeepakKalgudi@borqs.com>
2019-05-28 17:27:40 +0530

    dev: Add support for dsi2dpi interface and st7789v panel
    fbcon: Add support for different splash image sizes.
    gcdb: display: Add support for dsi2dpi interface and st7789v panel
    required for Feature phone hardware.
    
    Change-Id: Ib648183d741cd43851f31c8a906e48ae88579bc5
    Signed-off-by: Deepak Kalgudi <DeepakKalgudi@borqs.com>

--- dev/gcdb/display/rules.mk
+++ dev/gcdb/display/rules.mk
@@ -6,4 +6,5 @@ OBJS += \
     $(LOCAL_DIR)/gcdb_display.o \
     $(LOCAL_DIR)/gcdb_display_param.o \
     $(LOCAL_DIR)/panel_display.o \
-    $(LOCAL_DIR)/gcdb_autopll.o
+    $(LOCAL_DIR)/gcdb_autopll.o \
+    $(LOCAL_DIR)/mipi_tc358762_dsi2dpi.o
