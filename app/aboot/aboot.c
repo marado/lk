3a491d0488e1495d528bc0e5d7d55d86304fc252
Ashish Bhimanpalliwar <abhiman@codeaurora.org>
2021-04-14 13:50:52 +0530

    app: aboot: Corrected check to prevent OOB read in loading splash image
    Corrected check to avoid OOB read while loading splash image.
    
    Change-Id: I06eff9c05a071d7707788dd896c0ad8b79da5b44

--- app/aboot/aboot.c
+++ app/aboot/aboot.c
@@ -2,7 +2,7 @@
  * Copyright (c) 2009, Google Inc.
  * All rights reserved.
  *
- * Copyright (c) 2009-2017, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2009-2021, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -3652,11 +3652,7 @@ int splash_screen_check_header(logo_img_header *header)
 		return -1;
 	if (header->width == 0 || header->height == 0)
 		return -1;
-	if ((UINT_MAX/512 >= header->blocks) && (header->blocks != 0)){
-		if (header->blocks*512 < header->width * header->height)
-			return -1;
-	}
-	else {
+	if (((header->blocks == 0) || (header->blocks > UINT_MAX/512))) {
 		return -1;
 	}
 	return 0;
@@ -3725,6 +3721,11 @@ int splash_screen_flash()
 					(fb_display->bpp / 8), 4096);
 		uint32_t splash_size = ((((header->width * header->height *
 					fb_display->bpp/8) + 511) >> 9) << 9);
+
+		if ((header->height * header->width * (fb_display->bpp/8)) > (header->blocks * 512)) {
+			dprintf(CRITICAL, "ERROR: Splash image size invalid\n");
+			return -1;
+		}
 
 		if (splash_size > fb_size) {
 			dprintf(CRITICAL, "ERROR: Splash image size invalid\n");
@@ -3832,6 +3833,10 @@ int splash_screen_mmc()
 
 			realsize =  header->width * header->height * fb_display->bpp / 8;
 			readsize =  ROUNDUP((realsize + LOGO_IMG_HEADER_SIZE), blocksize) - blocksize;
+			if (realsize > (header->blocks * 512)) {
+				dprintf(CRITICAL, "Logo Size error\n");
+				return -1;
+			}
 
 			if (blocksize == LOGO_IMG_HEADER_SIZE) { /* read the content directly */
 				if (mmc_read((ptn + LOGO_IMG_HEADER_SIZE), (uint32_t *)base, readsize)) {
